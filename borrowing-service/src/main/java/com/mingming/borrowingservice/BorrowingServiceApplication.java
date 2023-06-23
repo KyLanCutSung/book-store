16fba5e2734e90809a27487ccc976357cc06e8a8
KyLanCutSung <nhatminh.tran.104855@gmail.com>
2023-06-23 10:57:17 +0700

    Tạo borrowing service, config lại các service

--- borrowing-service/src/main/java/com/mingming/borrowingservice/BorrowingServiceApplication.java
+++ borrowing-service/src/main/java/com/mingming/borrowingservice/BorrowingServiceApplication.java
@@ -1,9 +1,14 @@
 package com.mingming.borrowingservice;
 
+import com.mingming.borrowingservice.config.AxonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@Import({AxonConfig.class})
 public class BorrowingServiceApplication {
 
 	public static void main(String[] args) {
--- employee-service/src/main/java/com/mingming/employeeservice/command/controller/EmployeeCommandController.java
+++ employee-service/src/main/java/com/mingming/employeeservice/command/controller/EmployeeCommandController.java
@@ -1,32 +1,21 @@
 package com.mingming.employeeservice.command.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+
 import com.mingming.employeeservice.command.command.CreateEmployeeCommand;
 import com.mingming.employeeservice.command.command.DeleteEmployeeCommand;
 import com.mingming.employeeservice.command.command.UpdateEmployeeCommand;
 import com.mingming.employeeservice.command.model.EmployeeRequestModel;
-import com.netflix.discovery.converters.Auto;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.messaging.Source;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.messaging.MessageChannel;
-import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1/employees")
-@EnableBinding(Source.class)
 public class EmployeeCommandController {
     @Autowired
     private CommandGateway commandGateway;
-
-    @Autowired
-    private MessageChannel output;
 
     @PostMapping
     public String addEmployee(@RequestBody EmployeeRequestModel model){
@@ -47,15 +36,5 @@ public class EmployeeCommandController {
         DeleteEmployeeCommand command = new DeleteEmployeeCommand(employeeId);
         commandGateway.sendAndWait(command);
         return "emmployee deleted";
-    }
-    @PostMapping("/sendMessage")
-    public void sendMessage(@RequestBody String message){
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-            String json = mapper.writeValueAsString(message);
-            output.send(MessageBuilder.withPayload(json).build());
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
     }
 }
